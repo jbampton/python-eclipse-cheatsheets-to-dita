ad54ac032c1481254d43c4496c07d919a75eb745
John Bampton <jbampton@users.noreply.github.com>
2016-07-18 05:26:19 +1000

    update

--- transform.py
+++ transform.py
@@ -1,56 +1,123 @@
-try:
-    from lxml import etree
+import xml.etree.cElementTree as etree
+print("running with cElementTree on Python 2.5+")
 
-    print("running with lxml.etree")
-except:
-    try:
-        # Python 2.5
-        import xml.etree.cElementTree as etree
-        print("running with cElementTree on Python 2.5+")
+stylesheet="""<xsl:stylesheet
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:xs="http://www.w3.org/2001/XMLSchema"
+  xmlns:debrief="http://www.debrief.info/"
+  exclude-result-prefixes="xs debrief"
+  version="1.0">
 
-        ditamap = etree.XML('''<?xml version="1.0"?>
-        <!DOCTYPE map PUBLIC '-//OASIS//DTD DITA Map//EN' 'map.dtd'>
-        <map>
-            <title>Debrief Python Eclipse cheat sheets to DITA</title>
-            <topicmeta>
-                <searchtitle>Eclipse cheat sheets to PDF</searchtitle>
-                <shortdesc>Document Publishing Adventures with the DITA Open Toolkit</shortdesc>
-                <author>Debrief</author>
-                <author>John Bampton</author>
-                <source>http://debrief.info/</source>
-                <publisher>Github John Bampton</publisher>
-                <critdates>
-                    <created date='s'/>
-                </critdates>
-                <audience type='programmer' job='troubleshooting' experiencelevel='expert'/>
-                <category>Java</category>
-                <category>Document Publishing</category>
-                <othermeta name='Publishing' content='DITA'/>
-            </topicmeta>
+  <xsl:template match="/compositeCheatsheet">
+    <xsl:apply-templates select="taskGroup"/>
+  </xsl:template>
 
-        </map>
-        ''')
+  <xsl:template match="taskGroup">
+    <task id="{generate-id(.)}">
+      <title>
+        <xsl:apply-templates select="@name"/>
+      </title>
+      <shortdesc>
+        <xsl:apply-templates select="intro"/>
+      </shortdesc>
+      <xsl:choose>
+        <xsl:when test="taskGroup">
+          <xsl:apply-templates select="taskGroup"/>
+        </xsl:when>
+        <xsl:otherwise>
+          <xsl:for-each select="task">
+            <xsl:variable name="path" select="param[@name='path']/@value"/>
+            <task id="{generate-id(.)}">
+              <title>
+                <xsl:apply-templates select="document($path)//cheatsheet/@title"/>
+              </title>
+              <shortdesc>
+                <xsl:apply-templates select="document($path)//cheatsheet/intro/description"/>
+              </shortdesc>
+              <taskbody>
+                <steps>
+                  <xsl:apply-templates select="document($path)//cheatsheet"/>
+                </steps>
+                <result>
+                  <xsl:apply-templates select="onCompletion"/>
+                </result>
+              </taskbody>
+            </task>
+          </xsl:for-each>
+        </xsl:otherwise>
+      </xsl:choose>
+    </task>
+  </xsl:template>
+
+  <xsl:template match="cheatsheet">
+    <xsl:for-each select="item">
+      <step id="{generate-id(.)}">
+        <cmd>
+          <xsl:apply-templates select="@title"/>
+        </cmd>
+        <info>
+          <xsl:apply-templates select="description"/>
+        </info>
+        <stepxmp>
+          <xsl:choose>
+            <xsl:when test="@skip = 'true'">optional</xsl:when>
+          </xsl:choose>
+        </stepxmp>
+      </step>
+    </xsl:for-each>
+  </xsl:template>
+
+  <xsl:template match="b">
+   <b><xsl:apply-templates/></b>
+  </xsl:template>
+
+  <xsl:template match="text()">
+    <xsl:copy/>
+  </xsl:template>
+
+  <xsl:template match="i[.= 'NoPrint']"/>
+
+  <xsl:template match="text()[preceding-sibling::i[.='NoPrint'][1]][following-sibling::i[.='NoPrint'][1] ] | *[preceding-sibling::i[.='NoPrint'][1]][following-sibling::i[.='NoPrint'][1] ]"/>
+
+  <xsl:template match="br">
+    <ph></ph>
+  </xsl:template>
+
+  <xsl:template match="i">
+    <xsl:apply-templates/>
+  </xsl:template>
+
+</xsl:stylesheet>"""
 
 
-        # Write the XML to the output file
-        with open('map.ditamap', 'w') as output_file:
-            print(etree.tostring(ditamap))
-            output_file.write(etree.tostring(ditamap))
 
-    except ImportError:
-        try:
-            # Python 2.5
-            import xml.etree.ElementTree as etree
-            print("running with ElementTree on Python 2.5+")
-        except ImportError:
-            try:
-                # normal cElementTree install
-                import cElementTree as etree
-                print("running with cElementTree")
-            except ImportError:
-                try:
-                    # normal ElementTree install
-                    import elementtree.ElementTree as etree
-                    print("running with ElementTree")
-                except ImportError:
-                    print("Failed to import ElementTree from any known place")
+
+
+ditamap = etree.XML('''<?xml version="1.0"?>
+<!DOCTYPE map PUBLIC '-//OASIS//DTD DITA Map//EN' 'map.dtd'>
+<map>
+    <title>Debrief Python Eclipse cheat sheets to DITA</title>
+    <topicmeta>
+        <searchtitle>Eclipse cheat sheets to PDF</searchtitle>
+        <shortdesc>Document Publishing Adventures with Python</shortdesc>
+        <author>Debrief</author>
+        <author>John Bampton</author>
+        <source>http://debrief.info/</source>
+        <publisher>Github John Bampton</publisher>
+        <critdates>
+            <created date='s'/>
+        </critdates>
+        <audience type='programmer' job='troubleshooting' experiencelevel='expert'/>
+        <category>Java</category>
+        <category>Document Publishing</category>
+        <othermeta name='Publishing' content='DITA'/>
+    </topicmeta>
+
+</map>
+''')
+
+
+# Write the XML to the output file
+with open('output/map.ditamap', 'w') as output_file:
+    print(etree.tostring(ditamap))
+    output_file.write(etree.tostring(ditamap))
